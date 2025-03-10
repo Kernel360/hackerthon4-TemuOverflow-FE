5839636d0250767e89e0e5c02a81bff83003c173
yyysolhhh <solbaram37@gmail.com>
2025-03-10 18:02:49 +0900

    chore: tailwind.config.ts 커스텀 컬러 설정
    - indigo, tumbleweed 시리즈 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  // module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          '50': '#f2f6fb',
+          '100': '#e6eff9',
+          '200': '#d2e0f3',
+          '300': '#b7cbea',
+          '400': '#9aaedf',
+          '500': '#8093d4',
+          '600': '#5c6bc0',
+          '700': '#5661ac',
+          '800': '#48528b',
+          '900': '#3f4770',
+          '950': '#252941'
+        },
+        tumbleweed: {
+          '50': '#fdf5ef',
+          '100': '#f9e8db',
+          '200': '#f3cdb5',
+          '300': '#e9a47d',
+          '400': '#e18056',
+          '500': '#da6035',
+          '600': '#cc492a',
+          '700': '#aa3724',
+          '800': '#882e24',
+          '900': '#6e2820',
+          '950': '#3b120f'
+        }
+      }
+    }
+  },
+  plugins: []
+}
+
+export default config
