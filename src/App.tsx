63e2012d025119b2f1566ca7f742eec1ba001366
yyysolhhh <solbaram37@gmail.com>
2025-03-10 18:10:45 +0900

    feat: 게시글 목록 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import PostListPage from './pages/PostListPage'
+
+const Home = () => (
+  <div className="flex h-screen items-center justify-center">
+    <h1 className="text-3xl font-bold">홈 페이지</h1>
+  </div>
+)
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-4xl font-bold">App.tsx</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/"
+          element={<PostListPage />}
+        />
+        <Route
+          path="/posts"
+          element={<PostListPage />}
+        />
+        {/* 추후 구현할 다른 라우트들 */}
+        {/* <Route path="/posts/:id" element={<PostDetailPage />} /> */}
+        {/* <Route path="/posts/new" element={<NewPostPage />} /> */}
+      </Routes>
+    </Router>
   )
 }
+
+export default App
