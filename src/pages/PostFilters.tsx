822dda623a9ba1a7954257b27995ce4b2f44248b
yyysolhhh <solbaram37@gmail.com>
2025-03-11 15:04:55 +0900

    style: 버튼, 검색창 수직 정렬

--- src/pages/PostFilters.tsx
+++ src/pages/PostFilters.tsx
@@ -8,42 +8,15 @@ interface PostFiltersProps {
   handleSearch: (e: React.FormEvent) => void
 }
 
-const PostFilters: React.FC<PostFiltersProps> = ({
+const PostFilters = ({
   searchTerm,
   setSearchTerm,
   filter,
   handleFilterChange,
   handleSearch
-}) => {
+}: PostFiltersProps) => {
   return (
-    <div className="mb-6 flex flex-col justify-between gap-4 sm:flex-row">
-      <form
-        onSubmit={handleSearch}
-        className="relative w-full sm:w-96">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-          placeholder="제목 또는 작성자 검색..."
-          className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-        />
-        <button
-          type="submit"
-          className="absolute top-2.5 right-3">
-          <svg
-            className="h-5 w-5 text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor">
-            <path
-              fillRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </form>
-
+    <div className="flex h-10 rounded sm:flex-row sm:items-center sm:justify-center">
       <div className="flex gap-2">
         <button
           onClick={() => handleFilterChange('all')}
@@ -73,6 +46,33 @@ const PostFilters: React.FC<PostFiltersProps> = ({
           미해결
         </button>
       </div>
+      <div className="mx-4"></div> {/* 여백 추가 */}
+      <form
+        onSubmit={handleSearch}
+        className="relative w-full sm:w-96">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          placeholder="제목 또는 작성자 검색..."
+          className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="absolute top-2.5 right-3">
+          <svg
+            className="h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor">
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </form>
     </div>
   )
 }
